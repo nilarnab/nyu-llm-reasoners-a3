98f3326a9c8b1b56935a4c07e59a97a00d25735e
nilarnab <nilarnabdebnath@gmail.com>
2026-04-13 19:43:21 -0400

    change made

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ response_correct = """
 Step 1: 74 - 45 = 29
 Step 2: 29 + 19 = 48
 
-\\boxed{3 \\times (84 / 3)}
+\\boxed{(74 -     45) + 120    }
 """
-gt = {"target": 84, "numbers": [84, 3, 3]}
+gt = {"target": 48, "numbers": [19, 74, 45]}
 print("Case 1 (correct):", question_only_reward_fn_format_countdown(response_correct, gt))
